11c1894aa7ed985d0918be00228812a38e94e848
Newton <5769156+iamnewton@users.noreply.github.com>
2025-11-24 15:10:25 -0800

    chore: 🔧  update next config with environment variables

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  },
+}
 
 export default nextConfig;
